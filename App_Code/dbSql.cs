82ebe291bdd16265977e749ba1c973fcf6f8f00c
merlin0117 <39073238+merlin0117@users.noreply.github.com>
2020-01-14 14:17:17 +0800

    Add files via upload

---
+++ App_Code/dbSql.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+/// <summary>
+/// dbSql 的摘要说明
+/// </summary>
+public class dbSql
+{
+
+   
+    static string connectionString = ConfigurationManager.AppSettings["calConn"];
+    public dbSql()
+    {
+
+        //
+        // TODO: 在此处添加构造函数逻辑
+        //
+    }
+    /// <summary>
+    /// 返回影响结果行数
+    /// </summary>
+    /// <param name="SQLString"></param>
+    /// <returns></returns>
+    public static int ExecuteSql(string SQLString)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            using (SqlCommand cmd = new SqlCommand(SQLString, connection))
+            {
+                try
+                {
+                    connection.Open();
+                    int rows = cmd.ExecuteNonQuery();
+                    return rows;
+                }
+                catch (System.Data.SqlClient.SqlException e)
+                {
+                    connection.Close();
+                    throw e;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 返回是否影响结果行数
+    /// </summary>
+    /// <param name="SQLString"></param>
+    /// <returns></returns>
+    public static bool boolSql(string SQLString,SqlParameter[] para)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            using (SqlCommand cmd = new SqlCommand(SQLString, connection))
+            {
+                try
+                {
+                    connection.Open();
+                    cmd.Parameters.AddRange(para);
+                    int rows = cmd.ExecuteNonQuery();
+                    if (rows > 0)
+                        return true;
+                    else return false;
+                }
+                catch (System.Data.SqlClient.SqlException e)
+                {
+                    connection.Close();
+                    throw e;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 执行一条计算查询结果语句，返回查询结果（object）。
+    /// </summary>
+    /// <param name="SQLString">计算查询结果语句</param>
+    /// <returns>查询结果（object）</returns>
+    public static object GetSingle(string SQLString)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            using (SqlCommand cmd = new SqlCommand(SQLString, connection))
+            {
+                try
+                {
+                    connection.Open();
+                    object obj = cmd.ExecuteScalar();
+                    if ((Object.Equals(obj, null)) || (Object.Equals(obj, System.DBNull.Value)))
+                    {
+                        return null;
+                    }
+                    else
+                    {
+                        return obj;
+                    }
+                }
+                catch (System.Data.SqlClient.SqlException e)
+                {
+                    connection.Close();
+                    throw e;
+                }
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// 执行查询语句，返回DataSet
+    /// </summary>
+    /// <param name="SQLString">查询语句</param>
+    /// <returns>DataSet</returns>
+    public static DataSet Query(string SQLString)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                connection.Open();
+                SqlDataAdapter command = new SqlDataAdapter(SQLString, connection);
+                command.Fill(ds, "ds");
+            }
+            catch (System.Data.SqlClient.SqlException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return ds;
+        }
+    }
+
+    ///参数化查询
+    /// <summary>
+    /// 执行查询语句，返回DataSet
+    /// </summary>
+    /// <param name="SQLString">查询语句</param>
+    /// <returns>DataSet</returns>
+    public static DataSet Query(string SQLString, SqlParameter[] para)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                connection.Open();
+
+                using (SqlCommand com = new SqlCommand(SQLString, connection))
+                {
+                    com.Parameters.AddRange(para);
+                    SqlDataAdapter command = new SqlDataAdapter(com);
+                    command.Fill(ds, "ds");
+                }
+            }
+            catch (System.Data.SqlClient.SqlException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return ds;
+        }
+    }
+
+
+
+
+
+}
